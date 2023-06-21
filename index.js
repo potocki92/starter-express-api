8b495d1c1e4137e6b2d1a04dcf20a0ea25cb7baf
potocki92 <40219157+potocki92@users.noreply.github.com>
2023-06-21 19:07:13 +0000

    update

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-const express = require('express')
-const app = express()
+const serverless = require("serverless-http");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const routes = require("./routes.js");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const uri =
+`mongodb+srv://satanista:satanista123@cluster0.zsrttvb.mongodb.net/invoices_database`;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const corsOptions = {
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
