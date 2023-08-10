6c1ceae0b0a28b1ffdb44866da4303bbf965971c
potocki92 <mateusz.potocki92@gmail.com>
2023-08-10 23:01:59 +0200

    google auth

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const routes = require("./routes.js");
 const dotenv = require("dotenv");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const session = require("express-session");
 
 dotenv.config();
-const uri =
-`mongodb+srv://satanista:satanista123@cluster0.zsrttvb.mongodb.net/invoices_database`;
+const uri = `mongodb+srv://satanista:satanista123@cluster0.zsrttvb.mongodb.net/invoices_database`;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: "*", 
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
@@ -26,6 +28,60 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
+app.use(
+  session({
+    secret: "my-secret-key",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL:
+        "https://incom-fff0742e5ae9.herokuapp.com/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+  )
+);
+
+// Serializacja i deserializacja użytkownika
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+// Trasy serwera
+app.get("/", (req, res) => {
+  res.send("Serwer autoryzacji Google na Heroku");
+});
+
+// Inicjalizacja logowania przez Google
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+// Obsługa zwrotu z Google
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    // Zalogowano pomyślnie, możesz przekierować gdziekolwiek chcesz
+    res.redirect("/");
+  }
+);
 app.use(`/`, routes);
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
