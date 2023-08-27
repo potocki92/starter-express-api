0c780a8b733184cfa8d3e74d05e545b1e77f0cbf
potocki92 <mateusz.potocki92@gmail.com>
2023-08-27 19:52:05 +0200

    model/change
    * I added types to user schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,28 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  user: { type: Object, default: {} },
+  user: {
+    type: Object,
+    name: { type: String, require: false },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      unique: true,
+    },
+    NIP: { type: Number, required: false },
+    REGON: { type: Number, required: false },
+    address: {
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      street: { type: String, required: true },
+    },
+    phone: { type: Number, required: false },
+  },
   products: { type: Array, default: [] },
   clients: { type: Array, default: [] },
   invoices: { type: Array, default: [] },
